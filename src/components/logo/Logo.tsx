a43b24853458151ef1a3d37479eaf23a28d6ff07
Adriano Interaminense <adriano.interaminense@gmail.com>
2023-04-15 23:06:15 -0300

    Improve responsiveness UX #2

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -10,7 +10,7 @@ export function Logo() {
       <div className="logo__image">
         <img src={profile?.imageURL} alt="profile" />
       </div>
-      <div>
+      <div className="logo__content">
         <div className="logo__first-name">{profile?.firstName}</div>
         <div className="logo__last-name">{profile?.lastName}</div>
       </div>
